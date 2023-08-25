99597bab8a03a360af891cd72cffc13b2b0bb8ec
jdelpino78 <89923646+jdelpino78@users.noreply.github.com>
2023-08-25 10:26:06 -0400

    add Image Slider

---
+++ Image Slider with HTML, CSS and jQuery/estilo.js
@@ -0,0 +1,39 @@
+$(function() {
+
+    var inWrap = $('.inner-wrapper');
+  
+    $('.prev').on('click', function() {
+  
+      inWrap.animate({left: '0%'}, 300, function(){
+  
+        inWrap.css('left', '-100%');
+  
+        $('.slide').first().before($('.slide').last());
+  
+      });
+  
+  
+    });
+  
+  
+  
+    $('.next').on('click', function() {
+  
+      inWrap.animate({left: '-200%'}, 300, function(){
+  
+        inWrap.css('left', '-100%');
+  
+        $('.slide').last().after($('.slide').first());
+  
+      });
+  
+  
+    });
+
+    setInterval(function() {
+       $('.slide').last().fadeIn(2000).after($('.slide').first())        
+    }, 5000);
+  
+  
+  })
+  
